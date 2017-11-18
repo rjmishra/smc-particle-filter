24424d3e6eb17a6a0a0ede6b2d53133c61e5cc2d
Ranjan Mishra <rjmishra111@gmail.com>
2017-11-19 02:17:44 +0530

    Nothing really done,, just adding two almost empty files.
     Changes to be committed:
    	new file:   smc-partice-filter/core/instance.cpp
    	new file:   smc-partice-filter/utils/csv_reader.cpp

---
+++ smc-partice-filter/core/instance.cpp
@@ -0,0 +1,17 @@
+/*
+ * Author : Ranjan Mishra
+ * Desc   : Placeholder class for data instances 
+ * Date   : 19th Nov 2017
+ */
+
+class instance{
+    private : 
+    std::vector<double> single_instance;
+    unsigned num_of_attributes;
+
+
+    public :
+    // public methods go here
+
+}
+
---
+++ smc-partice-filter/utils/csv_reader.cpp
@@ -0,0 +1,7 @@
+/*
+ * Author : Ranjan Mishra
+ * Desc   : Utility function implementation for reading data from csv file
+ * Date   : 19th Nov 2017
+ */
+
+
